fb3f98f38fef3a08039f733f92145ba92437b7ec
Leckar <gusleckar@gmail.com>
2022-06-29 14:45:57 -0300

    initial commit

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -5,7 +5,7 @@ function countEntrants(entrants) {
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  // seu código aqui hehe
 }
 
 module.exports = { calculateEntry, countEntrants };
