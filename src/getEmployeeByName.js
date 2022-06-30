f6ec7131349a4b698c5aa88d70099b3b5bf9edb8
Leckar <gusleckar@gmail.com>
2022-06-30 18:28:38 -0300

    completed up to requisite 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName) {
+    const { employees } = data;
+    return employees.find((e) => (e.firstName === employeeName || e.lastName === employeeName));
+  }
+  return {};
 }
 
 module.exports = getEmployeeByName;
