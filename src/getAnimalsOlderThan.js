f9d104a9946e01a129cf0a77464984e8ba64149c
Leckar <gusleckar@gmail.com>
2022-06-30 18:23:14 -0300

    completed up to requisite 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -3,7 +3,7 @@ const data = require('../data/zoo_data');
 function getAnimalsOlderThan(animal, age) {
   const { species } = data;
   const target = species.find((inspect) => (inspect.name === animal));
-  return target.residents.every((e) => (e.age > age));
+  return target.residents.every((e) => (e.age >= age));
 }
 
 module.exports = getAnimalsOlderThan;
