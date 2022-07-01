9e117bd9c47a6a2376f0dcc9b56c6fa06b8d2728
Leckar <gusleckar@gmail.com>
2022-06-30 22:29:49 -0300

    completed up to requisite 9

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,58 @@
 const data = require('../data/zoo_data');
 
+function paramCheck(param) {
+  const { hours, species } = data;
+  if (Object.keys(hours).includes(param)) {
+    return 'day';
+  }
+  if (species.some((e) => e.name === param)) {
+    return 'animal';
+  }
+  return false;
+}
+function animalSchedules(target) {
+  const { species } = data;
+  const targetSpecies = species.find((e) => e.name === target);
+  return targetSpecies.availability;
+}
+
+function animalFinder(targetDay) {
+  const { species } = data;
+  return species.filter((e) => e.availability.includes(targetDay)).map((e) => e.name);
+}
+function daySchedules(target) {
+  if (target === 'Monday') {
+    return { Monday: { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' } };
+  }
+  const { hours } = data;
+  const day = hours[target];
+  const schedule = {};
+  schedule[target] = { officeHour: `Open from ${day.open}am until ${day.close}pm`,
+    exhibition: animalFinder(target) };
+  return schedule;
+}
+function fullSchedule() {
+  const schedule = {};
+  const { hours } = data;
+  const dayList = Object.keys(hours);
+  dayList.forEach((day) => {
+    const newObj = daySchedules(day);
+    schedule[day] = newObj[day];
+  });
+  return schedule;
+}
+console.log(fullSchedule());
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  const check = paramCheck(scheduleTarget);
+  switch (check) {
+  case 'day':
+    return daySchedules(scheduleTarget);
+  case 'animal':
+    return animalSchedules(scheduleTarget);
+  default:
+    break;
+  }
+  return fullSchedule();
 }
 
 module.exports = getSchedule;
