fbd58020a47203df7332b8e2cb8051b4e24c9950
Leckar <gusleckar@gmail.com>
2022-06-30 23:23:07 -0300

    completed up to requisite 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const { employees, species } = data;
+  const firstAnimal = employees.find((e) => e.id === id).responsibleFor[0];
+  const { residents } = species.find((e) => e.id === firstAnimal);
+  const z = residents.sort((a, b) => b.age - a.age)[0];
+  return [z.name, z.sex, z.age];
 }
 
 module.exports = getOldestFromFirstSpecies;
